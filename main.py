6a9b611169f7118ae451e8d818f2df2cc3aed87a
tarinikonanki <tarini.konanki@gmail.com>
2025-11-05 23:52:16 +0000

    Finished level 1 and started working on level 2

--- main.py
+++ main.py
@@ -5,11 +5,39 @@ from datetime import datetime, timedelta
 # TODO: Create a function to view all books that are currently available
 # Output should include book ID, title, and author
 
+#Understand: I need to print out the books in library_books.py, making sure to show book ID, title, and author
+# - Input is going to be a list (which is library_books), and output is going to be a String of all the books and their info; edge cases are if library_books list doesn't have all the info/blank info
+#Clues: I likely will need to use a for loop as I'm going to traverse through all of library_books.py. I will also have to use string methods and fstrings as I'm printing it out.
+#Assemble:
+
+#FUNCTION viewAllBooks (input library_books)
+    #Print "Here are all of the books in the library: " + new line
+    #FOR each book in library_books
+        #IF Available is equal to true THEN
+            #print book ID + title + authore 
+
+def viewAllBooks(library_books):
+    print("Here are all of the books in the library: \n")
+    for book in library_books:
+        if book.get("available") == True:
+            print(f"{book.get("id")} - {book.get("title")} by {book.get("author")}")
+
+viewAllBooks(library_books)
+
+
 
 # -------- Level 2 --------
 # TODO: Create a function to search books by author OR genre
 # Search should be case-insensitive
 # Return a list of matching books
+
+#Understand: I have to create a function where the user can search library_books by either author or genre
+# - Inputs = string stating author or genre; Outputs list of books with that author or genre
+#Clues: As it involves searching, I will need to use for loops. I might also need to use string methods to make it case sensitive.
+#Assemble:
+
+#DEFINE function bookSearch - input String searchValue
+    #FOR each book in library_books
 
 
 # -------- Level 3 --------
