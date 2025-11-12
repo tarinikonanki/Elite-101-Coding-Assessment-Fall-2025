aedcb8c56dad008c6f9670d6ba18b4cb5a33930a
tarinikonanki <tarini.konanki@gmail.com>
2025-11-12 05:08:18 +0000

    Finished level 3

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ viewAllBooks(library_books)
 # Return a list of matching books
 
 #Understand: I have to create a function where the user can search library_books by either author or genre
-# - Inputs = string stating author or genre; Outputs list of books with that author or genre
+# - Inputs = string stating author or genre; Outputs list of books with that author or genre; Edge cases is if the book is not found in library books
 #Clues: As it involves searching, I will need to use for loops. I might also need to use string methods to make it case sensitive.
 #Assemble:
 
@@ -44,6 +44,8 @@ viewAllBooks(library_books)
         #print "Books by author searchValue"
     #ELSE IF searchValue in genre_list
         #print "Books in genre searchValue"
+    #ELSE
+        #PRINT "Book is not found in library"
     #FOR each book in library_books
         #IF book author equals searchValue or book genre equals searchValue
             #print book
@@ -55,9 +57,12 @@ def bookSearch(searchValue):
         print("Here are books by author " + string.capwords(searchValue) + ":")
     elif any(genre.casefold() == searchValue.casefold() for genre in genre_list):
         print("Here are books in the " + string.capwords(searchValue) + " genre:")
+    else:
+        print("There is no book with this information in this library.")
     for book in library_books:
         if book.get("author").casefold() == searchValue or book.get("genre").casefold() == searchValue:
             print(f"Book ID: {book.get("id")} - {book.get("title")} by {book.get("author")} - Genre: {book.get("genre")} - Is it Available? {book.get("available")} - Due Date: {book.get("due date")} - Number of Checkouts: {book.get("checkouts")}")
+
 
 bookSearch("fantasy")
 bookSearch("ray bradbury")
@@ -71,6 +76,57 @@ bookSearch("ray bradbury")
 #   - Increment the checkouts counter
 # If it is not available:
 #   - Print a message saying it's already checked out
+
+#Understand: I have to create a function where the user can check a book based on its ID
+# - Inputs = string stating book ID; Outputs either that you checked out the book or you can't as it is already checked out; edge cases include if the ID is invalid/doesn't match any book
+#Clues: Since I will need search through the library books list, I will need to use for loops. I might also need to use string methods to make my searches case sensitive.
+#Assemble:
+
+#DEFINE function checkoutBook - input String ID
+    #IF ID exists in library
+        #FOR each book in library_books
+            #IF book ID equals ID
+                #PRINT "Book to checkout: book"
+                #IF book is available
+                    #available is set to FALSE
+                    #IF due date is currently none
+                        #set due date to today's date plus two weeks
+                    #ELSE IF due date has a value
+                        #due date is date plus two weeks
+                    #checkouts equals checkouts plus one
+                #ELSE if book is not available
+                    #PRINT "The book is already checked out"
+    #ELSE if ID does not exist library
+        #PRINT "There is no book in the library with that ID."
+
+def checkoutBook(ID):
+    if any(book.get("id").casefold() == ID.casefold() for book in library_books):
+        for book in library_books:
+            if book.get("id").casefold() == ID.casefold():
+                print(f"Book to checkout: {book.get("title")} by {book.get("author")}")
+                if book.get("available") == True:
+                    book["available"] = False
+                    if(book.get("due date") == None):
+                        book["due date"] = datetime.now().date() + timedelta(weeks=2)
+                    else:
+                        book["due date"] = book["due date"] + timedelta(weeks=2)
+                    book["checkouts"] = book["checkouts"] + 1
+                    print(f"You just checked out {book.get("title")} by {book.get("author")} - ID: {book.get("id")} - Due Date: {book.get("due date")} - Number of Checkouts: {book.get("checkouts")} - Currently Available: {book.get("available")}.")
+                else:
+                    print(f"{book.get("title")} by {book.get("author")} - ID {ID} is already checked out.")
+    else:
+        print(f"Sorry, there is no book in the library with the book ID {ID}.")
+
+checkoutBook("B3")
+checkoutBook("B2")
+checkoutBook("B4")
+checkoutBook("C4")
+
+
+
+
+
+
 
 
 # -------- Level 4 --------
